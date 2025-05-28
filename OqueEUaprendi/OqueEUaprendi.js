8f3cf25832eb106d384c7922a1cff623cf75211d
FrederaFilipina <frederafilipina@gmail.com>
2025-05-28 15:17:32 -0300

    Ajuste geral no repositório

---
+++ OqueEUaprendi/OqueEUaprendi.js
@@ -0,0 +1,9 @@
+function mostrar_menuPessoalExtentido(){
+    document.getElementById("menuPessoalRecolhido").style.display = "flex"
+    document.getElementById("menuPessoalExtentido").style.display = "flex"
+}
+
+function esconder_menuPessoalExtentido(){
+    document.getElementById("menuPessoalRecolhido").style.display = "flex"
+    document.getElementById("menuPessoalExtentido").style.display = "none"
+}
