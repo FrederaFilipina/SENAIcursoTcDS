549d4a6e34809fca964200471f5bf28714de4f72
FrederaFilipina <FrederaFiliipna@gmail.com>
2025-05-29 20:55:53 -0300

    Ajustes

---
+++ Organizar/CRUD/ExAvancado-1/ExAv.1-TelaCadastro.js
@@ -0,0 +1,22 @@
+
+function mostrarLogin(){
+    esconderTodas()
+    document.getElementById('login').style.display = 'flex'
+}
+
+function mostrarCadastro(){
+    esconderTodas()
+    document.getElementById('cadastro').style.display = 'flex'
+
+}
+
+function mostrarProdutos(){
+    esconderTodas()
+    document.getElementById('produtos').style.display = 'flex'
+}
+
+function esconderTodas(){
+    document.getElementById('login').style.display = 'none'
+    document.getElementById('cadastro').style.display = 'none'
+    document.getElementById('produtos').style.display = 'none'
+}
