3f78bff4d8776e5b158a03740720ebe2a01af764
FrederaFilipina <FrederaFiliipna@gmail.com>
2025-05-29 22:11:51 -0300

    Ajustes

---
+++ OqueEUaprendi/OqueEUaprendi_V0.js
@@ -0,0 +1,9 @@
+function mostrar_menuPessoalEstendido(){
+    document.getElementById("menuPessoalRecolhido").style.display = "flex"
+    document.getElementById("menuPessoalEstendido").style.display = "flex"
+}
+
+function esconder_menuPessoalEstendido(){
+    document.getElementById("menuPessoalRecolhido").style.display = "flex"
+    document.getElementById("menuPessoalEstendido").style.display = "none"
+}
--- Organizar/CRUD/ExAvancado-1/ExAv.1-TelaCadastro.js
+++ Organizar/CRUD/ExAvancado-1/ExAv.1-TelaCadastro.js
@@ -1,22 +1,68 @@
+// let nome, email, senha
 
-function mostrarLogin(){
+let usuario = {
+    nome: `um`,
+    email: `dois`,
+    senha: `nuvem`
+}
+
+function cadastrar() {
+    usuario.nome = document.getElementById('inpCadNome').value
+    usuario.email = document.getElementById('inpCadEmail').value
+    usuario.senha = document.getElementById('inpCadSenha').value
+    alert(`Cadastro com sucesso!! :D`)
+
+    console.log(usuario)
+    limparInputs()
+    mostrarLogin()
+}
+
+function logar() {
+    let nome = document.getElementById('inpLogNome').value
+    let senha = document.getElementById('inpLogSenha').value
+
+    if (nome === usuario.nome && senha === usuario.senha) {
+        alert(`Parabéns ${nome}, Login efetuado com sucesso! :P`)
+        limparInputs()
+        mostrarProdutos()
+    } else {
+        alert(`Login ERRADO!`)
+    }
+
+}
+
+
+
+function mostrarLogin() {
     esconderTodas()
     document.getElementById('login').style.display = 'flex'
+    document.getElementById("inpLogNome").focus()
 }
 
-function mostrarCadastro(){
+function mostrarCadastro() {
     esconderTodas()
     document.getElementById('cadastro').style.display = 'flex'
+    document.getElementById("inpCadNome").focus()
 
 }
 
-function mostrarProdutos(){
+function mostrarProdutos() {
     esconderTodas()
     document.getElementById('produtos').style.display = 'flex'
 }
 
-function esconderTodas(){
+function esconderTodas() {
     document.getElementById('login').style.display = 'none'
     document.getElementById('cadastro').style.display = 'none'
     document.getElementById('produtos').style.display = 'none'
+}
+
+function limparInputs(){
+    document.getElementById('inpCadNome').value=""
+    document.getElementById('inpCadEmail').value=""
+    document.getElementById('inpCadSenha').value=""
+
+    document.getElementById('inpLogNome').value=""
+    document.getElementById('inpLogSenha').value=""
+
 }
