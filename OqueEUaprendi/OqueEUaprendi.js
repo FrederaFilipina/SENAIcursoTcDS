1b97e7d7f2fa2cc412d541fe22cce3eaf3f78eb7
FrederaFilipina <frederafilipina@gmail.com>
2025-07-26 17:51:42 -0300

    Ajustes de final de Semestre

--- OqueEUaprendi/OqueEUaprendi.js
+++ OqueEUaprendi/OqueEUaprendi.js
@@ -1,11 +1,53 @@
-function mostrarInf() {
-    document.getElementById('infPessoalEstendido').style.display = "flex"
-    document.getElementById('infPessoalRecolhido').style.display = "none"
+const lista = [
+    {
+        nome: `HTML`,
+        lst: [`A`, `B`, `C`]
+    },
+
+    {
+        nome: `JS`,
+        lst: [`D`, `E`, `F`]
+    },
+
+    {
+        nome: `CSS`,
+        lst: [`G`, `H`, `I`]
+    }
+]
+
+function limparGeral() {
+    document.querySelector('.area-lousa').innerHTML = ``
+    document.querySelector('.menu-Lista').innerHTML = ``
 }
 
-function esconderInf() {
-    document.getElementById('infPessoalEstendido').style.display = "none"
-    document.getElementById('infPessoalRecolhido').style.display = "flex"
+function listaConteudo(parametro) {
+    limparGeral()
+    let conteudoNome = parametro
+    let nomeConteudo
+    nomeConteudo = lista.find(nomeLst => nomeLst.nome === conteudoNome)
+
+    document.querySelector('.menu-Lista').innerHTML =
+        `<div class="lista-NomeConteudo">
+            <span>Lista de conteudo sobre ${nomeConteudo.nome}</span>
+        </div>
+
+    <div class="lista-LstConteudo">
+    </div>`
+
+    let cC = 0
+    while (cC != nomeConteudo.lst.length) {
+        document.querySelector('.lista-LstConteudo').innerHTML +=
+            `<div onclick="abrirConteudoLousa('${nomeConteudo.lst[cC]}')" id="lstConteudo-Nome">
+                ${nomeConteudo.lst[cC]}
+            </div>`
+        cC++
+    }
 }
 
-setInterval(esconderInf, 7000)
+function abrirConteudoLousa(cntdLousa) {
+    let lousaCntd = cntdLousa
+
+    document.querySelector('.area-lousa').innerHTML =
+        `${lousaCntd}`
+
+}
