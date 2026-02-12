e578330ccba9f785fc982a1a69a86cff5b413752
FrederaFilipina <FrederaFilipina@gmail.com>
2026-02-11 22:17:48 -0300

    Aula um revisão SQL e JS

--- SEMESTRE.3/Desenvolvimento de Sistemas/BackEnd/aula01-revisao/src/bibliotecaService.js
+++ SEMESTRE.3/Desenvolvimento de Sistemas/BackEnd/aula01-revisao/src/bibliotecaService.js
@@ -4,4 +4,22 @@ export async function buscarLivrosPorId(livroId) {
     const [rows] = await pool.query("SELECT * from livros WHERE id=?", [livroId])
     console.log(rows)
     return rows[0]
+};
+
+export async function buscarview() {
+    const [rows] = await pool.query("SELECT * from vw_livros")
+    console.log(rows)
+    return rows    
+};
+
+export async function cadastrarLivro(titulo, categoria, valor_unitario, estoque_minimo, estoque_maximo) {
+    const [rows] = await pool.query( 'INSERT INTO livros (titulo, categoria, valor_unitario, estoque_minimo, estoque_maximo) VALUES(?,?,?,?,?)', [titulo, categoria, valor_unitario, estoque_minimo, estoque_maximo])
+    console.log(rows)
+    return rows
+}
+
+export async function livrosMaiorSaida() {
+    comst [rows] = await pool.query
+    console.log(rows)
+    return rows
 }
--- SEMESTRE.3/Desenvolvimento de Sistemas/BackEnd/aula01-revisao/src/index.js
+++ SEMESTRE.3/Desenvolvimento de Sistemas/BackEnd/aula01-revisao/src/index.js
@@ -1,9 +1,12 @@
-import { buscarLivrosPorId } from "./bibliotecaService.js";
+import { buscarLivrosPorId, buscarview, cadastrarLivro } from "./bibliotecaService.js";
 import {pool} from "./config.js";
 
 async function  main(){
     await buscarLivrosPorId(1)
+    await buscarview()
+    await cadastrarLivro('Teste','Testinho', '10', '1', '10')
 }
+
 main(). catch(error =>
     console.error(error)
 ). finally(async()=>{
