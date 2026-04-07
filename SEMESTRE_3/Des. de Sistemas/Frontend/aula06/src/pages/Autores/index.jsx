65de45b724586974ec59a06c58b1d1bc282ec54c
frederafilipina <frederafilipina@gmail.com>
2026-04-07 14:54:37 -0300

    Backend: Atv-Autores

--- SEMESTRE_3/Des. de Sistemas/Frontend/aula06/src/pages/Autores/index.jsx
+++ SEMESTRE_3/Des. de Sistemas/Frontend/aula06/src/pages/Autores/index.jsx
@@ -16,23 +16,31 @@ const Autores = () => {
 
     return (
         <>
-            <div className='flex gap-2'>
+            <div className="flex flex-wrap gap-2 px-2 py-2">
                 {
                     posts.map(post => (
-                        <div key={post.id} className='card'>
-                            <img src={post.image} alt={post.title} />
-                            <h2>{post.title}</h2>
-                            <h3>{post.views}</h3>
-                            <p>{post.description}</p>
-                            <Link to={`/post/${post.id}`} className='text-white bg-blue-500'>
-                            ver mais...
-                            </Link>
+                        <div key={post.id} className="bg-gray-900 text-white rounded-2xl overflow-hidden w-72 shadow-lg hover:scale-105 transition duration-300">
+                            <div className="relative">
 
+                                <img src={post.foto} alt={post.nome} className="w-full aspect-[4/3] object-cover opacity-80"/>
 
-                           
+                                <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent"></div>
+                                <h2 className="absolute bottom-2 left-3 text-lg font-bold">{post.nome}</h2>
+
+                            </div>
+
+                            <div className="p-4 flex flex-col gap-2">
+
+                                <h3 className="text-sm text-gray-300">{post.descricao}</h3>
+                                <p className="text-xs text-gray-400">Nascimento: {post.cidade}</p>
+
+                                <Link to={`/post/${post.id}`} className="mt-2 bg-white text-gray-900 text-sm px-3 py-1 rounded-lg text-center font-semibold hover:bg-gray-200 transition">
+                                    Ver mais....
+                                </Link>
+                            </div>
                         </div>
                     )
-                )
+                    )
                 }
             </div>
         </>
